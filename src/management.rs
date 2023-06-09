157367c0f1b808f416fd5cea562bfbf83fd90219
meteor <liuzechen.coder@qq.com>
2023-06-09 16:51:44 +0800

    管理员的1

--- src/management.rs
+++ src/management.rs
@@ -7,6 +7,7 @@ use mysql::prelude::*;
 use mysql::*;
 
 use std::collections::HashMap;
+use std::fmt::format;
 pub fn first_management() {
     unsafe {
         if number::IS_MANAGE_FIST_TIME {
@@ -67,6 +68,35 @@ pub fn admin_manage() {
         return;
     }
     println!("欢迎使用管理员系统");
+    loop {
+        println!("选项 1 不同职称的教师数量");
+        println!("选项 2 不同职称的教师的平均工资");
+        println!("选项 3 按班级的成绩列表");
+        println!("选项 4 某位学生的学分");
+        println!("选项 5 退出");
+        let a = tools::scan().parse::<i32>();
+
+        // 判断错误
+        if a.is_err() {
+            println!("{}", number::INPUT_ERR_MESSAGE);
+            continue;
+        }
+        // 判断受否是其他数字
+        let number = a.unwrap();
+        if number < 1 || number > 5 {
+            println!("{}", number::INPUT_ERR_MESSAGE);
+            continue;
+        }
+        match number {
+            1 => admin_manage_one_unique_teacher_count(),
+            2 => admin_manage_two_unique_average_teacher_salayr(),
+            3 => admin_manage_three_list_by_class(),
+            4 => admin_manage_four_student_list(),
+            _ => {
+                return;
+            }
+        }
+    }
 }
 pub fn student_manage() {
     let (user_id, password) = tools::scanf_username_and_password();
@@ -219,5 +249,66 @@ fn super_admin_manage_three_look_teacher_class() {
     }
 }
 fn super_admin_manage_four_add_student() {
-    println!("请输入当前函数")
+    println!("请输入当前学生的id");
+    let user_id = tools::scan();
+    let sql = format!("select * from student  where id = '{}'", user_id);
+    let mut have_this_user: bool = false;
+    let pool = mysql::Pool::new(number::URL).unwrap(); // 获取连接池
+    let mut connect = pool.get_conn().unwrap(); // 获取连接
+    connect.query_iter(sql).unwrap().for_each(|row| {
+        let _r: (String, String, i32, i32, i32, String) = from_row(row.unwrap());
+        have_this_user = true;
+    });
+    if have_this_user {
+        println!("有这名同学的信息了,拒接访问");
+        return;
+    }
+    println!("请输入学生姓名");
+    let user_name = tools::scan();
+    println!("请输入学生性别 1 表示 男 0 表示 女");
+    let user_sex = tools::scan();
+    println!("请输入当前学生的年龄");
+    let user_age = tools::scan();
+    println!("请输入当学生的班级");
+    let user_class = tools::scan();
+    println!("请输入学生的初始化密码");
+    let user_password = tools::scan();
+    let sql: String = format!(
+        "insert into database_project.student(id, name, sex, age, class, password) values (
+        '{}', '{}', {}, {}, {}, '{}')",
+        user_id, user_name, user_sex, user_age, user_class, user_password
+    );
+    connect.query_iter(sql).unwrap(); // 这个是插入
+
+    println!("插入成功!");
 }
+
+/*
+    println!("选项 1 不同职称的教师数量");
+    println!("选项 2 不同职称的教师的平均工资");
+    println!("选项 3 按班级的成绩列表");
+    println!("选项 4 某位学生的学分");
+    println!("选项 5 退出");
+*/
+fn admin_manage_one_unique_teacher_count() {
+    let sql: String = String::from("select * from teacher");
+    let mut teacher_count: HashMap<String, i32> = HashMap::new();
+    let pool = mysql::Pool::new(number::URL).unwrap(); // 获取连接池
+    let mut connect = pool.get_conn().unwrap(); // 获取连接
+    connect.query_iter(sql).unwrap().for_each(|row| {
+        let r: (i32, String, i32, String, f64) = from_row(row.unwrap());
+        let profession: String = r.3.clone();
+        for class in profession.split_whitespace() {
+            let number = teacher_count.entry(class.to_string()).or_insert(0);
+            *number += 1;
+        }
+    });
+    println!("以下是不同职称的数量");
+    for (key, val) in &teacher_count {
+        println!("{}\t{}", key, val);
+    }
+    
+}
+fn admin_manage_two_unique_average_teacher_salayr() {}
+fn admin_manage_three_list_by_class() {}
+fn admin_manage_four_student_list() {}
